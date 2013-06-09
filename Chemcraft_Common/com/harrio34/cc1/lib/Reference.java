2bf1cd7a3cb15ec5ecda89d10434d8113c84596f
harrio34 <ha.freni@me.com>
2013-06-09 15:57:41 -0700

    Added Initial Mod Files

---
+++ Chemcraft_Common/com/harrio34/cc1/lib/Reference.java
@@ -0,0 +1,8 @@
+package com.harrio34.cc1.lib;
+
+public class Reference 
+{
+	public static final String MOD_ID = "CC1";
+	public static final String MOD_NAME = "ChemCraft1";
+	public static final String VERSION ="0.0.1";
+}
