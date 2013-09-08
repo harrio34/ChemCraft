43a763ab08615c1772945ecaf8e6f56c0f23e5be
harrio34 <ha.freni@me.com>
2013-09-08 14:09:14 -0700

    Added new Files

--- Chemcraft_Common/com/harrio34/cc1/ChemCraft.java
+++ Chemcraft_Common/com/harrio34/cc1/ChemCraft.java
@@ -1,31 +1,166 @@
 package com.harrio34.cc1;
 
+import java.io.File;
+import java.util.Arrays;
+
+import com.harrio34.cc1.core.proxy.CommonProxy;
 import com.harrio34.cc1.lib.Reference;
 
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraftforge.common.MinecraftForge;
+
+import cpw.mods.fml.common.IFuelHandler;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.Mod.PostInit;
 import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.event.FMLFingerprintViolationEvent;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLInterModComms;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.event.FMLInterModComms.IMCEvent;
+import cpw.mods.fml.common.network.NetworkRegistry;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.TickRegistry;
+import cpw.mods.fml.relauncher.Side;
 
+/**
+ * Chem-Craft1
+ * 
+ * ChemCraft
+ * 
+ * @author harrio34
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ * 
+ */
+
+@SuppressWarnings("deprecation")
 @Mod( modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class ChemCraft 
 {
-	@PreInit
-	public void preInit(FMLPreInitializationEvent event)
-	{
-	
-	}
-	@Init
-	public void init(FMLInitializationEvent event)
-	{
-		
-	}
-	@PostInit
-	public void postInit(FMLPostInitializationEvent event)
-	{
-		
-	}
+	@Instance(Reference.MOD_ID)
+    public static ChemCraft instance;
+
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+    public static CommonProxy proxy;
+
+    public static CreativeTabs tabsEE3 = new CreativeTabCC1(CreativeTabs.getNextID(), Reference.MOD_ID);
+
+    @EventHandler
+    public void invalidFingerprint(FMLFingerprintViolationEvent event) {
+
+        // Report (log) to the user that the version of Equivalent Exchange 3
+        // they are using has been changed/tampered with
+        if (Reference.FINGERPRINT.equals("@FINGERPRINT@")) {
+            LogHelper.warning(Strings.NO_FINGERPRINT_MESSAGE);
+        }
+        else {
+            LogHelper.severe(Strings.INVALID_FINGERPRINT_MESSAGE);
+        }
+    }
+
+    @EventHandler
+    public void serverStarting(FMLServerStartingEvent event) {
+
+        // Initialize the custom commands
+        CommandHandler.initCommands(event);
+    }
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+        // Initialize the log helper
+        LogHelper.init();
+
+        // Initialize the configuration
+        ConfigurationHandler.init(new File(event.getModConfigurationDirectory().getAbsolutePath() + File.separator + Reference.CHANNEL_NAME + File.separator + Reference.MOD_ID + ".cfg"));
+
+        // Conduct the version check and log the result
+        VersionHelper.execute();
+
+        // Initialize the Version Check Tick Handler (Client only)
+        TickRegistry.registerTickHandler(new VersionCheckTickHandler(), Side.CLIENT);
+
+        // Initialize the Render Tick Handler (Client only)
+        proxy.registerRenderTickHandler();
+
+        // Register the KeyBinding Handler (Client only)
+        proxy.registerKeyBindingHandler();
+
+        // Register the Sound Handler (Client only)
+        proxy.registerSoundHandler();
+
+        // Initialize mod blocks
+        ModBlocks.init();
+
+        // Initialize mod items
+        ModItems.init();
+    }
+
+    @EventHandler
+    @SuppressWarnings("unchecked")
+    public void load(FMLInitializationEvent event) {
+
+        // Register the GUI Handler
+        NetworkRegistry.instance().registerGuiHandler(instance, proxy);
+
+        // Register the PlayerDestroyItem Handler
+        MinecraftForge.EVENT_BUS.register(new PlayerDestroyItemHandler());
+
+        // Register the Item Pickup Handler
+        MinecraftForge.EVENT_BUS.register(new ItemEventHandler());
+
+        // Register the EntityLiving Handler
+        MinecraftForge.EVENT_BUS.register(new EntityLivingHandler());
+
+        MinecraftForge.EVENT_BUS.register(new ActionRequestHandler());
+
+        MinecraftForge.EVENT_BUS.register(new WorldTransmutationHandler());
+
+        GameRegistry.registerCraftingHandler(new CraftingHandler());
+
+        // Register the DrawBlockHighlight Handler
+        proxy.registerDrawBlockHighlightHandler();
+
+        // Initialize mod tile entities
+        proxy.registerTileEntities();
+
+        // Initialize custom rendering and pre-load textures (Client only)
+        proxy.initRenderingAndTextures();
+
+        // Add in the ability to dye Block/Item
+        CraftingManager.getInstance().getRecipeList().add(new Item());
+
+        // Register the Fuel Handler
+        GameRegistry.registerFuelHandler(new FuelHandler());
+
+        // Quick test to see that sending an encoded recipe to be added to the
+        // recipe registry works
+        FMLInterModComms.sendMessage(Reference.MOD_ID, InterModComms.ADD_RECIPE, NBTHelper.encodeRecipeAsNBT(Item.bucketWater, Arrays.asList(Item.bucketEmpty, Block.waterStill)));
+        FMLInterModComms.sendMessage(Reference.MOD_ID, InterModComms.ADD_RECIPE, NBTHelper.encodeRecipeAsNBT(Item.bucketLava, Arrays.asList(Item.bucketEmpty, Block.lavaStill)));
+    }
+
+    @EventHandler
+    public void modsLoaded(FMLPostInitializationEvent event) {
+
+        // Initialize the Addon Handler
+        AddonHandler.init();
+
+        // Initialize the DynEMC system
+        DynEMC dynEMC = DynEMC.getInstance();
+    }
+
+    @EventHandler
+    public void handleIMCMessages(IMCEvent event) {
+
+        InterModCommsHandler.processIMCMessages(event);
+    }
 }
--- Chemcraft_Common/com/harrio34/cc1/lib/Reference.java
+++ Chemcraft_Common/com/harrio34/cc1/lib/Reference.java
@@ -5,4 +5,14 @@ public class Reference
 	public static final String MOD_ID = "CC1";
 	public static final String MOD_NAME = "ChemCraft1";
 	public static final String VERSION ="0.0.1";
+	public static final String CLIENT_PROXY_CLASS = "com.harrio34.CC1.core.proxy.ClientProxy";
+	public static final String SERVER_PROXY_CLASS = "com.harrio34.CC1.core.proxy.CommonProxy";
+    public static final String CHANNEL_NAME = MOD_ID;
+    public static final String DEPENDENCIES = "required-after:Forge@[9.10.1.849,)";
+    public static final String FINGERPRINT = "@FINGERPRINT@";
+    public static final int SECOND_IN_TICKS = 20;
+    public static final int SHIFTED_ID_RANGE_CORRECTION = 256;
+    public static final int VERSION_CHECK_ATTEMPTS = 3;
+
+    public static final int ORE_DICTIONARY_NOT_FOUND = -1;
 }
